f10b4b8d884fe48a4b972d9d347b31e36a1ff190
carol <crosezhang@gmail.com>
2022-09-25 23:20:20 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 const BASE_URL = "https://dataservice.accuweather.com";
-const API_KEY = "USE_YOUR_API_KEY"; // terrible practice!
+const API_KEY = "YwUQofcX7Lr8gaySg26TpOAUBErYGceu"; // terrible practice!
 // You should never save API key directly in source code
 
 const search = document.getElementById("search");
@@ -9,24 +9,39 @@ search.addEventListener("submit", getWeatherForecast);
 
 function getWeatherForecast(event) {
   event.preventDefault();
-  const city = document.getElementById("city").value.trim();
+  const city = document.getElementById("city").value.trim(); // removes spaces
   getLocationKey(city);
 }
 
+// get the "location key" for the given `city`!
+// then call getCurrentCondition to retrieve weather forecast for it!
 function getLocationKey(city) {
-  // TODO get the "location key" for the given `city`!
-  //  then call getCurrentCondition to retrieve weather forecast for it!
-  console.log(city);
+  fetch(`${BASE_URL}/locations/v1/cities/search?apikey=${API_KEY}&q=${city}`)
+    .then((response) => response.json()) // asynchronous programming
+    .then((data) => {
+     const location = data[0];
+     getCurrentCondition(location);
+    }) // returning a promise
+    .catch((err) => console.log(err));
 }
 
+// get the "current condition" based on the `location` argument!
+// then call updateUI to update the UI!
 function getCurrentCondition(location) {
-  // TODO get the "current condition" based on the `location` argument!
-  //  then call updateUI to update the UI!
+  const key = location.Key;
+  fetch(`${BASE_URL}/currentconditions/v1/${key}?apikey=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      updateUI(location, data[0]);
+    })
+    .catch((err) => console.log(err));
 }
 
 function updateUI(location, forecast) {
-  // TODO update the following based on `location` and `forecast` arguments!
-  document.getElementById("name").innerText = "City Name";
-  document.getElementById("condition").innerText = "Weather Condition";
-  document.getElementById("temperature").innerText = "Temperature";
+  const cityName = location.LocalizedName;
+  const condition = forecast.WeatherText;
+  const temperature = forecast.Temperature.Imperial.Value;
+  document.getElementById("name").innerText = `${cityName}`;
+  document.getElementById("condition").innerText = condition;
+  document.getElementById("temperature").innerText = `${temperature} F`;
 }
